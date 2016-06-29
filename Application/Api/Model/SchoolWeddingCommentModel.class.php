92c3332318ea2e6ad459400d1869bfe415c8d078
huzhang <1354720365@qq.com>
2016-06-29 16:15:23 +0800

    婚礼头条列表，详情，评论，回复接口

---
+++ Application/Api/Model/SchoolWeddingCommentModel.class.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/6/28
+ * Time: 18:34
+ */
+
+namespace Api\Model;
+
+use Think\Model;
+
+class SchoolWeddingCommentModel extends Model{
+    //自动完成
+    protected  $_auto =array(
+        array('create_time','time',Model::MODEL_INSERT,'function'),
+        array('update_time','time',Model::MODEL_BOTH,'function'),
+        array('status','1',Model::MODEL_INSERT,'string')
+    );
+
+     //自动验证
+    protected $_validate =array(
+        array('content','require','请填写评论内容！',Model::MUST_VALIDATE,'regex',Model::MODEL_BOTH),
+    );
+
+
+
+}
