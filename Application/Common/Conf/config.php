7f2f759d121d796b13f48e02e38a10ab3dff4319
wtwei <wutwei@qq.com>
2016-07-13 12:26:50 +0800

    删除冗余代码

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -22,29 +22,6 @@ return array(
         // '/^(\w+)(?:\/(\w+))$/' => 'Home/:1/:2',
     ),
 
-    /* 模块相关配置 */
-    'AUTOLOAD_NAMESPACE' => array('Addons' => ONETHINK_ADDON_PATH), //扩展模块列表
-    'DEFAULT_MODULE'     => 'Home',
-    'MODULE_DENY_LIST'   => array('Common','User','Admin','Install'),
-    //'MODULE_ALLOW_LIST'  => array('Home','Admin'),
-
-    /* 系统数据加密设置 */
-    'DATA_AUTH_KEY' => 'iVFW$~gO"<M,K^!*h_Itm(/Q:cns.8`SGlrj[D#R', //默认数据加密KEY
-    'DATA_CACHE_KEY'=> 'iVFW$~gO"<M,K^!*h_Itm(/Q:cns.8`SGlrj[D#R',
-
-    /* 用户相关设置 */
-    'USER_MAX_CACHE'     => 1000, //最大缓存用户数
-    'USER_ADMINISTRATOR' => 1, //管理员用户ID
-
-    /* URL配置 */
-    'URL_CASE_INSENSITIVE' => true, //默认false 表示URL区分大小写 true则表示不区分大小写
-    'URL_MODEL'            => 3, //URL模式
-    'VAR_URL_PARAMS'       => '', // PATHINFO URL参数变量
-    'URL_PATHINFO_DEPR'    => '/', //PATHINFO URL分割符
-
-    /* 全局过滤配置 */
-    'DEFAULT_FILTER' => '', //全局过滤函数
-
     /* 数据库配置 */
     'DB_TYPE'   => 'mysql', // 数据库类型
     'DB_HOST'   => '112.124.16.5', // 服务器地址
