5099b083c116012133648cf70a1550fd273f4bdc
huzhang <1354720365@qq.com>
2016-08-31 17:50:30 +0800

    社区登录删除

--- Application/Api/Controller/PublicController.class.php
+++ Application/Api/Controller/PublicController.class.php
@@ -49,54 +49,7 @@ class PublicController extends CommonController {
     }
 
 
-    /**
-     * 微社区注册/登录
-     */
-    public function MicroCommunityLogin($data){
-        $ua = $_SERVER['HTTP_USER_AGENT'];
-        if (strpos($ua, 'iPhone') || strpos($ua, 'iPad')) {
-            //$ak = '57624435e0f55ab83b000868';
-            //$key = 'f1040c987c3ca653985b4c486e560b67';
-            $ak = C('IOS_AK');
-            $key = C('IOS_SECRET');
-        } else {
-            //$ak = '57624411e0f55ab83b000848';
-            //$key = '65115406623996afcc0a14f2e4d00c7f';
-            $ak = C('ANDROID_AK');
-            $key = C('ANDROID_SECRET');
-        }
-
-
-        //封装用户信息
-        //$custom['company'] =$data['company'];
-        //$custom['position'] =$data['position'];
-        //$custom['truename'] =$data['truename'];
-        //$custom_json = json_encode($custom);
-
-
-        $temp = array(
-            'user_info'=>array(
-                'name'=>$data['username'],
-                'icon_url'=>$data['avatar'],
-            ),
-            'source_uid'=> (string)$data['id'],
-            'source'=>'self_account',
-        );
-
-        $data = json_encode($temp);
-
-        // 加密
-        $data = pack("N",strlen($data)).$data;
-        $string = encrypt($data, $key);
-        $encrypted_data = base64_encode($string);
-
-
-
-        $url = 'https://rest.wsq.umeng.com/0/get_access_token?ak=' . $ak;
-        $result = curl_post($url, array('encrypted_data'=>$encrypted_data));
-
-        return isset($result['access_token']) ? array('uid'=>$result['id'], 'access_token'=>$result['access_token']) : array('uid'=>'', 'access_token'=>'');
-    }
+    
 
 
 
