33d5c25a54761c59d5b60ae7d6fb94c5b82fe3d9
野猪yz <1746534101@qq.com>
2016-09-05 10:38:04 +0800

    合并

--- src/Ke/app/entry.js
+++ src/Ke/app/entry.js
@@ -1,36 +1,14 @@
-import App from './components/App'
 import Detail from './modules/detail'
 import store from './store'
-import Todos from './components/todos'
 import User from './modules/User'
 import Index from './modules/index'
 
 
-
-// The Reducer Function
-// var userReducer = function(state = [], action) {
-//   if (action.type === 'ADD_USER') {
-//     var newState = state.concat([action.user]);
-//     return newState;
-//   }
-//   return state;
-// };
-
-
-// Create a store by passing in the reducer
-// var store = Redux.createStore(userReducer);
-
-
-//ReactDOM.render(
-//  <App />,
-//  document.getElementById('root')
-//);
-
 var Router=ReactRouter.Router
 var Route=ReactRouter.Route
-var hashHistory=ReactRouter.hashHistory
 var browserHistory=ReactRouter.browserHistory
 var Provider=ReactRedux.Provider
+
 
 
 let reactElement = document.getElementById('root')
