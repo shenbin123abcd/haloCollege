1d3ad4a4942499c4c0ed9e03f2892ba067d979e8
wtwei <wutwei@qq.com>
2016-09-06 22:07:11 +0800

    增加课程接口

--- Application/Ke/Controller/PayController.class.php
+++ Application/Ke/Controller/PayController.class.php
@@ -17,17 +17,47 @@ class PayController extends CommonController {
         empty($course_id) && $this->error('课程编号错误');
 
         $course = M('Course')->where(array('id'=>$course_id, 'status'=>1))->find();
-        empty($course) || ($course['start_data'] < time()) && $this->error('课程不存在或已经结束');
+        empty($course) || ($course['start_date'] < time()) && $this->error('课程不存在或已经结束');
 
-        vendor('Pay.Payment');
+        // 创建订单
+        $model = M('CourseOrder');
+        $order = $model->where(array('wechat_id'=>$this->user['id'], 'course_id'=>$course_id, 'status'=>0))->find();
+        if (!empty($order) && $order['exp_time'] > time()) {
+            $this->success(unserialize($order['sign']));
+        }else{
+            $model->where(array('id'=>$order['id']))->save(array('status'=>2));
+            $order = array();
+        }
 
-        $pay = new Payment('wxmp');
-        $pay->setNotify('http://ke.halobear.com/course/notify');
+        if (empty($order)){
+            $body = $course['title'] . '-' . $course['city'];
+            $order['wechat_id'] = $this->user['id'];
+            $order['course_id'] = $course_id;
+            $order['body'] = $body;
+            $order['order_no'] = 'KE' . date("d") . rand(10,99) . implode(explode('.', microtime(1)));
+            $order['exp_time'] = time() + 6900;
+            $order['create_time'] = time();
+            $order['price'] = $course['price'];
+            $order['type'] = 0;
+            $order['status'] = 0;
+            $order['sign'] = '';
 
-        $body = $course['title'] . '-' . $course['city'];
+            $order_id = $model->add($order);
 
-        echo $sign = $pay->sign(['subject'=>$body, 'body'=>$body, 'order_no'=>'', 'amount'=>'0.01']);
+            vendor('Pay.Payment');
 
-        //$this->success($sign);
+            $pay = new Payment('wxmp');
+            $pay->setNotify('http://ke.halobear.com/course/notifyn');
+
+            $sign = $pay->sign(['subject'=>$body, 'body'=>$body, 'order_no'=>$order['order_no'], 'amount'=>$order['price'], 'openid'=>$this->user['openid']]);
+
+            if ($sign['iRet']){
+                $model->where(array('id'=>$order_id))->setField('sign', serialize($sign['data']));
+                $this->success($sign['data']);
+            }else{
+                $this->error($sign['info']);
+            }
+        }
+
     }
 }
