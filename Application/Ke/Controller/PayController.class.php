64e4e8fd92bc86b4650c706c14af013dfd1d4158
wtwei <wutwei@qq.com>
2016-09-07 14:28:37 +0800

    增加用户报名检查

--- Application/Ke/Controller/PayController.class.php
+++ Application/Ke/Controller/PayController.class.php
@@ -19,8 +19,12 @@ class PayController extends CommonController {
         $course = M('Course')->where(array('id'=>$course_id, 'status'=>1))->find();
         empty($course) || ($course['start_date'] < time()) && $this->error('课程不存在或已经结束');
 
-        // 创建订单
         $model = M('CourseOrder');
+        // 检查用户是否已经报名
+        $count = $model->where(array('wechat_id'=>$this->user['id'], 'course_id'=>$course_id, 'status'=>1))->count();
+        $count && $this->error('你已经报过名了，不能重复报名');
+
+        // 创建订单
         $order = $model->where(array('wechat_id'=>$this->user['id'], 'course_id'=>$course_id, 'status'=>0))->find();
         if (!empty($order) && $order['exp_time'] > time()) {
             $this->success(unserialize($order['sign']));
@@ -47,7 +51,7 @@ class PayController extends CommonController {
             vendor('Pay.Payment');
 
             $pay = new Payment('wxmp');
-            $pay->setNotify('http://ke.halobear.com/course/notifyn');
+            $pay->setNotify('http://ke.halobear.com/pay/notifyn');
 
             $sign = $pay->sign(['subject'=>$body, 'body'=>$body, 'order_no'=>$order['order_no'], 'amount'=>$order['price'], 'openid'=>$this->user['openid']]);
 
@@ -59,5 +63,41 @@ class PayController extends CommonController {
             }
         }
 
+    }
+
+    public function notifyn(){
+        vendor('Pay.Payment');
+        $pay = new Payment('wxmp');
+        $notify = $pay->wxmpVerify();
+
+        $response = $notify->payment->handleNotify(function($notify, $successful){
+            // 使用通知里的 "微信支付订单号" 或者 "商户订单号" 去自己的数据库找到订单
+            $order = M('CourseOrder')->where(array('order_no'=>$notify['out_trade_no']))->find();
+            if (!$order) { // 如果订单不存在
+                return 'Order not exist.';
+            }
+            // 如果订单存在
+            // 检查订单是否已经更新过支付状态
+            if ($order['pay_time']) {
+                return true;
+            }
+            // 用户是否支付成功
+            if ($successful) {
+                // 不是已经支付状态则修改为已经支付状态
+                $data['pay_time'] = time();
+                $data['status'] = 1;
+                $data['transaction_id'] = $notify['transaction_id'];
+                $ret = M('CourseOrder')->where(array('id'=>$order['id']))->save($data);
+
+                if ($ret){
+                    M('Course')->where(array('id'=>$order['course_id']))->setInc('num');
+                }
+            } else { // 用户支付失败
+                write_log('pay_mpwx_error' . date('Ymd'), var_export($notify, 1));
+                write_log('pay_mpwx_error' . date('Ymd'), var_export($successful, 1));
+            }
+            return true; // 返回处理完成
+        });
+        $response->send();
     }
 }
