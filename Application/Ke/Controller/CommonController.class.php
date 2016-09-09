a7abdcf1ed7c68fabd49a18f14af99935618f3f1
野猪yz <1746534101@qq.com>
2016-09-09 15:48:18 +0800

    status->iRet

--- Application/Ke/Controller/CommonController.class.php
+++ Application/Ke/Controller/CommonController.class.php
@@ -10,7 +10,7 @@ class CommonController extends Controller {
      * 初始化操作
      */
     public function _initialize() {
-        //cookie('halobear','MDk0YnhkNFJaWlA5cmVubkg3bmZBb2VoWVRFRjd6WkM5T05YZjdxUm91ZFJYTUdrSzlySURoOFNOWEY3M3FkMW9jNEo0a0FEZUVLR285UXU0bTQ2VVdhaDQ3emhKQkFtaHc=', 864000);
+//        cookie('halobear','MDk0YnhkNFJaWlA5cmVubkg3bmZBb2VoWVRFRjd6WkM5T05YZjdxUm91ZFJYTUdrSzlySURoOFNOWEY3M3FkMW9jNEo0a0FEZUVLR285UXU0bTQ2VVdhaDQ3emhKQkFtaHc=', 864000);
         $this->_getWechatUser();
     }
 
@@ -26,7 +26,7 @@ class CommonController extends Controller {
             cookie('halobear', null, -86400);
             $url = 'http://ke.halobear.com/courses/wechat?url=' . urlencode('http://ke.halobear.com' . $_SERVER['REQUEST_URI']);
             if (IS_AJAX) {
-                $this->ajaxReturn(array('status'=>-1,'info'=>'No authorization token was found', 'data'=>$url));
+                $this->ajaxReturn(array('iRet'=>-1,'info'=>'No authorization token was found', 'data'=>$url));
             }else{
                 redirect($url);
             }
