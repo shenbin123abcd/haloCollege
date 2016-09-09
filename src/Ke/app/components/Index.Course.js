382f46071dae28c3cfd5f926e05e6fffee1f3da4
野猪yz <1746534101@qq.com>
2016-09-09 13:03:59 +0800

    index no data

--- src/Ke/app/components/Index.Course.js
+++ src/Ke/app/components/Index.Course.js
@@ -13,7 +13,13 @@ const Course = ({isFetching,items, renderItem}) => {
     }
 
     if (isEmpty) {
-        return <div>no data</div>
+        return (
+            <div className="index-no-data-wrapper">
+                <div className="bg-ico"></div>
+                <div className="main-msg">正在筹备中，时刻关注!</div>
+                <div className="second-msg">精选课程马上上线，请耐心等候哦</div>
+            </div>
+        )
     }
 
     return (
