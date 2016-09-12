cf2957151709de951f104c43543ab9adc7f0a3f2
wtwei <wutwei@qq.com>
2016-09-12 15:36:35 +0800

    修改金熊奖购买记录

--- Application/Ke/Controller/UcController.class.php
+++ Application/Ke/Controller/UcController.class.php
@@ -11,18 +11,12 @@ class UcController extends CommonController {
 
     // 案例支付成功记录
     public function caseRecord(){
-        $list = M('wfc2016_order_case')->where(array('record_id'=>$this->user['id'], 'status'=>1))->field('order_no,goods_name,goods_id,goods_subtitle,goods_cover,goods_url,type,price,module,spec,num')->order('id DESC')->select();
+        $list = M('wfc2016_order_case')->where(array('record_id'=>$this->user['id'], 'status'=>1))->field('order_no,goods_name,goods_id,goods_subtitle,goods_cover,goods_url,type,price,module,spec,num,pay_time')->order('id DESC')->select();
         foreach ($list as $key => $value) {
             // 样片
-            if ($value['module'] == 'case') {
-                $list[$key]['type'] = $value['type'] == '98' ? '基础款' : '高级款';
-                $list[$key]['send_date'] = '2016-08-30';
-            }elseif ($value['module'] == 'book') {
+            if ($value['module'] == 'book') {
                 $list[$key]['type'] = '';
-                $list[$key]['send_date'] = '2016-08-30';
-            }elseif ($value['module'] == 'daoju') {
-                $list[$key]['type'] = $value['spec'];
-                $list[$key]['send_date'] = '';
+                $list[$key]['send_date'] = date('Y-m-d',$value['pay_time'] + 2*86400);
             }
         }
 
