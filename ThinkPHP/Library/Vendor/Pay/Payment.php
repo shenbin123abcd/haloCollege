d9ea35ebf1efc23360f513381959768b801881c0
wtwei <wutwei@qq.com>
2016-09-13 13:18:11 +0800

    修改支付方法

--- ThinkPHP/Library/Vendor/Pay/Payment.php
+++ ThinkPHP/Library/Vendor/Pay/Payment.php
@@ -79,7 +79,7 @@ class Payment {
         // payment
         'payment' => [
             'merchant_id'        => '1385772202',
-            'key'                => 'hxkj20090725hxkj2009072520090725',
+            'key'                => '227c6daa608c56b1eb06ed79f8d9c119',
             'cert_path'          => VENDOR_PATH.'Pay/wechat/api_cert.pem',
             'key_path'           => VENDOR_PATH.'Pay/wechat/api_key.pem',
             'notify_url'         => '',       // 你也可以在下单时单独设置来想覆盖它
@@ -191,7 +191,7 @@ class Payment {
         $order = new Order($attributes);
 
         // 下单
-        $result = $payment->prepare($order);dump($result);
+        $result = $payment->prepare($order);
         $config = false;
         if ($result->return_code == 'SUCCESS' && $result->result_code == 'SUCCESS'){
             $prepay_id = $result->prepay_id;
@@ -200,7 +200,7 @@ class Payment {
             $config = $payment->configForAppPayment($prepay_id);
         }
 
-        return $config;
+        return $config ? array('iRet'=>1, 'data'=>$config) : array('iRet'=>0, 'info'=>$result->return_msg);
     }
 
     private function wxmpSign($order){
@@ -252,29 +252,6 @@ class Payment {
     }
 
     private function wechatVerify(){
-        $app = new Application($this->wechat);
-        $response = $app->payment->handleNotify(function($notify, $successful){
-            // 使用通知里的 "微信支付订单号" 或者 "商户订单号" 去自己的数据库找到订单
-            $order = 查询订单($notify['out_trade_no']);
-            if (!$order) { // 如果订单不存在
-                return 'Order not exist.'; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
-            }
-            // 如果订单存在
-            // 检查订单是否已经更新过支付状态
-            if ($order->paid_at) { // 假设订单字段“支付时间”不为空代表已经支付
-                return true; // 已经支付成功了就不再更新了
-            }
-            // 用户是否支付成功
-            if ($successful) {
-                // 不是已经支付状态则修改为已经支付状态
-                $order->paid_at = time(); // 更新支付时间为当前时间
-                $order->status = 'paid';
-            } else { // 用户支付失败
-                $order->status = 'paid_fail';
-            }
-            $order->save(); // 保存订单
-            return true; // 返回处理完成
-        });
-        $response->send();
+        return new Application($this->wechat);
     }
 }
