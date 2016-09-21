4ed0c6be21b0eed3ac2c3433865e7253b7d5ab80
野猪yz <1746534101@qq.com>
2016-09-21 18:38:34 +0800

    BUG 6 课堂的详情也要选中月份

--- src/Ke/js/app.util.js
+++ src/Ke/js/app.util.js
@@ -1,6 +1,6 @@
 ;app.util=(function(){
     function formatTitle(str) {
-        let strArr=str.split('|')
+        let strArr=str.split(/\||丨/);
         return `${_.trim(strArr[0])}《${_.trim(strArr[1])}》`
     }
     return {
