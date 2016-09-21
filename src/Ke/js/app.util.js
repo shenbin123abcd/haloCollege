0183a105fe89b25c4969a3c7e1a8079ff199f8af
野猪yz <1746534101@qq.com>
2016-09-21 18:44:33 +0800

    微信分享

--- src/Ke/js/app.util.js
+++ src/Ke/js/app.util.js
@@ -1,6 +1,6 @@
 ;app.util=(function(){
     function formatTitle(str) {
-        if(str.indexOf(/\||丨/g)>-1){
+        if(str.indexOf("|")>-1||str.indexOf("丨")>-1){
             let strArr=str.split(/\||丨/g);
             return `${_.trim(strArr[0])}《${_.trim(strArr[1])}》`
         }else{
