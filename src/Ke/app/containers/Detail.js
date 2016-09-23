4cb0f9d77f93387050498df84eab7a97709191b6
shenbin <846697463@qq.com>
2016-09-23 14:53:59 +0800

    college/ke/detail修改样式

--- src/Ke/app/containers/Detail.js
+++ src/Ke/app/containers/Detail.js
@@ -101,7 +101,9 @@ var Detail= React.createClass({
                     <InterviewBlock interviewData={fetchData.video}></InterviewBlock>
                     <div className="bg-gap">
                         <div className="weixin-block">
-                            <img src={weixinPic} alt=""/>
+                            <div className="wrapper">
+                                <img src={weixinPic} alt=""/>
+                            </div>
                             <div className="text">长按二维码添加「 幻熊学院 」客服微信号<br/>咨询课程报名相关事项</div>
                         </div>
                     </div>
@@ -236,6 +238,7 @@ var ClassDesc=React.createClass({
       data.forEach(function(n,i){
           n.desc=descList[i];
       });
+      data[2].desc='11月01-12月02日';
 
       data[0].desc='￥'+data[0].desc+'/人';
       if(cate=='公开课'){
