72cf73fc3b8430b1112d2fb72fcd856a388897af
wtwei <wutwei@qq.com>
2016-10-17 16:02:50 +0800

    修改cookie生成规则

--- Application/Ke/Controller/IndexController.class.php
+++ Application/Ke/Controller/IndexController.class.php
@@ -7,7 +7,7 @@ class IndexController extends CommonController {
     public function index() {
         $code = I('code');
         $agents = cookie('agents');
-        if (!empty($code) && empty($agents)){
+        if (!empty($code)){ //  && empty($agents)
             // 检查code的有效性
             $check = M('CourseAgents')->where(['code'=>$code, 'status'=>1])->count();
 
