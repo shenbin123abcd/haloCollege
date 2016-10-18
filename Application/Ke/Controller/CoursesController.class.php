2b9fb7b4287598ecb0335b2b80171faedcd61c23
wtwei <wutwei@qq.com>
2016-10-18 09:38:41 +0800

    修改代理商提交

--- Application/Ke/Controller/CoursesController.class.php
+++ Application/Ke/Controller/CoursesController.class.php
@@ -225,7 +225,7 @@ class CoursesController extends CommonController {
         if ($agents){
             $this->error('你已经提交申请了，请耐心等待');
         }else{
-            M('CourseAgents')->add(['phone'=>$phone, 'create_time'=>time(), 'update_time'=>time()]);
+            M('CourseAgents')->add(['phone'=>$phone, 'create_time'=>time(), 'update_time'=>time(), 'code'=>uniqid()]);
             $this->success('', '提交成功，工作人员会在一个工作日内与您联系');
         }
     }
@@ -268,6 +268,11 @@ class CoursesController extends CommonController {
             $user = M('WechatAuth')->where(['openid'=>$agents['openid']])->field('nickname, headimgurl')->find();
         }
 
-        $user ? $this->success($user) : $this->error();
+        if(empty($user)){
+            cookie('agents', null, time()-86400);
+            $this->error();
+        }else{
+            $this->success($user);
+        }
     }
 }
