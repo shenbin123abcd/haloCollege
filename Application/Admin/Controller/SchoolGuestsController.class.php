9c5433885dc6a283405eab27b6dc0fa633bdf931
huzhang <1354720365@qq.com>
2016-10-25 11:45:35 +0800

    显示公司logo和用户头像

--- Application/Admin/Controller/SchoolGuestsController.class.php
+++ Application/Admin/Controller/SchoolGuestsController.class.php
@@ -9,6 +9,9 @@ class SchoolGuestsController extends CommonController {
 	public function _join(&$data){
 	//标识头像没上传的用户
 		$data = $this->is_headimg($data);
+	//获取公司logo
+		$data = $this->get_company_logo($data);
+
 	}
 
 	public function _before_edit(){
@@ -27,6 +30,9 @@ class SchoolGuestsController extends CommonController {
 		foreach ($data as $key=>$value){
 			if($value['avatar_url']==''){
 				$data[$key]['is_headimg'] = '<b style="color: red">未上传</b>';
+			}else{
+				$url = 'http://7xopel.com2.z0.glb.clouddn.com/'.$value['avatar_url'];
+				$data[$key]['is_headimg'] = '<img'.' '.'style='.'width:300px;height:100px'.' '.'src='.$url.' '.'/>';
 			}
 			if ($value['position']==''){
 				$data[$key]['position'] = '<b style="color: red">无职务</b>';
@@ -61,6 +67,34 @@ class SchoolGuestsController extends CommonController {
 		$api = C('AUTH_API_URL') . 'company?' . http_build_query($data);
 		$result = curl_get($api, $data);
 		return $result;
+	}
+
+	/**
+	 * 获取公司logo
+	*/
+	public function get_company_logo($data){
+		foreach ($data as $key=>$value){
+			$company_ids[]=$value['company_id'];
+		}
+		$company_ids = array_unique($company_ids);
+		if (!empty($company_ids)){
+			$where = array('id' =>array('in',$company_ids),'filter[store_id]' => 0);
+			$result = $this->company($where);
+			if (!empty($result['data']['data'])) {
+				foreach ($result['data']['data'] as $key => $value) {
+					$list[$value['id']] = $value['logo'];
+				}
+			} else {
+				$list = array();
+			}
+			foreach ($data as $key=>$value){
+				$url = 'http://7xopel.com2.z0.glb.clouddn.com/';
+				$data[$key]['company_logo'] = !empty($list[$value['company_id']]) ? '<img'.' '.'style='.'width:300px;height:100px'.' '.'src='.$url.$list[$value['company_id']].' '.'/>' : '<b style="color: red">未上传</b>';
+			}
+
+		}
+
+		return $data;
 	}
 
 }
