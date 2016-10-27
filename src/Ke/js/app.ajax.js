229f24e87907d33bd8e75023d344068012c6613b
shenbin <846697463@qq.com>
2016-10-27 18:53:13 +0800

    college

--- src/Ke/js/app.ajax.js
+++ src/Ke/js/app.ajax.js
@@ -1,4 +1,13 @@
 ;app.ajax=function( url , settings  ){
+    let params=hb.location.url("?wechat_code");
+    if(params){
+        $.ajax({
+            url:'/courses/setLogin',
+            data:{
+                wechat_code:params
+            }
+        })
+    }
     var ajax=$.ajax( url , settings ).then((res)=>{
         // console.log(res)
         if(res.iRet==-1){
