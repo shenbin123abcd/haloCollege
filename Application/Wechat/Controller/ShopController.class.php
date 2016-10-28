28c661178a977d13b7ae7ba89c6374df61cb04d1
wtwei <wutwei@qq.com>
2016-10-28 10:59:30 +0800

    增加微信单点登录

---
+++ Application/Wechat/Controller/ShopController.class.php
@@ -0,0 +1,31 @@
+<?php
+namespace Wechat\Controller;
+
+use Think\Controller;
+
+class ShopController extends Controller
+{
+    public function index()
+    {
+        $ret_url = $_SERVER['HTTP_REFERER'] ? $_SERVER['HTTP_REFERER'] : 'http://shop.halobear.cn';
+        $back = 'http://wechat.halobear.com/shop/returnClient?ret_url=' . urlencode($ret_url);
+        $url = 'http://wechat.halobear.com/?scope=snsapi_base&back=' . $back;
+        redirect($url);
+    }
+
+    public function returnClient(){
+        $ret_url = urldecode($_GET['ret_url']);
+        if (isset($_GET['wechat_code']) && !empty($_GET['wechat_code'])){
+            $code = decrypt(base64_decode(urldecode($_GET['wechat_code'])), C('WECHAT_AUTH_KEY'));
+            $parse = parse_url($ret_url);
+
+            if ($parse['query']){
+                $ret_url .= '&';
+            }else{
+                $ret_url .= '?';
+            }
+            $ret_url .= 'openid=' . $code['openid'];
+        }
+        redirect($ret_url);
+    }
+}
