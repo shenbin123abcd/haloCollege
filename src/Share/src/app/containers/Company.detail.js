dc9895d84d9740e1d201290a3cca5ee2236aaa47
merlingli <merlingli@163.com>
2016-11-09 19:17:12 +0800

    显示更多按钮

--- src/Share/src/app/containers/Company.detail.js
+++ src/Share/src/app/containers/Company.detail.js
@@ -13,6 +13,42 @@ class Company extends React.Component{
     componentDidMount(){
         const { dispatch,location,routeParams} = this.props;
     }
+    componentDidUpdate(){
+        var profile = this.refs.profile;
+        var hh = $(profile).outerHeight();
+        var moreInfo = this.refs.moreInfo;
+        var moreBtn = this.refs.moreBtn;
+        if(hh<40){
+            $(moreInfo).hide();
+            $(profile).css({
+                "display":"block",
+                "height":hh,
+            });
+        }else{
+            $(profile).css("display","-webkit-box");
+            var hh2 = $(profile).outerHeight();
+            $(profile).css({
+                "height":hh2,
+            });
+            $(moreInfo).show();
+            $(moreInfo).on("click",function () {
+                if($(moreInfo).text()=="查看更多"){
+                    $(moreInfo).text("收起");
+                    $(profile).css({
+                        "display":"block",
+                        "height":hh,
+                    });
+                }else if($(moreInfo).text()=="收起"){
+                    // var hh2 = $(profile).outerHeight();
+                    $(moreInfo).text("查看更多");
+                    $(profile).css({
+                        "display":"-webkit-box",
+                        "height":hh2,
+                        });
+                }
+            });
+        }
+    }
 
     render(){
         const { dispatch,location,companyDetailData } = this.props;
@@ -37,14 +73,14 @@ class Company extends React.Component{
                             </div>
                             <div className="myLine"></div>
                             <div className="company-profile">
-                                <div className="company-myprofile">公司简介：{`${companyDetailData.data.company.description}`}</div>
-                                <div className="company-more">
-                                    <div className="company-more-word">查看更多</div>
-                                    <div className="company-more-arrow">
-                                        <svg className="haloIcon" aria-hidden="true">
-                                            <use xlinkHref="#haloIcon-arrow"></use>
-                                        </svg>
-                                    </div>
+                                <div className="company-myprofile" ref="profile">公司简介：{`${companyDetailData.data.company.description}`}</div>
+                                <div className="company-more" ref="moreInfo">
+                                    <div className="company-more-word" ref="moreBtn">查看更多</div>
+                                    {/*<div className="company-more-arrow">*/}
+                                        {/*<svg className="haloIcon" aria-hidden="true">*/}
+                                            {/*<use xlinkHref="#haloIcon-arrow"></use>*/}
+                                        {/*</svg>*/}
+                                    {/*</div>*/}
                                 </div>
                             </div>
                         </div>
--- src/Share/src/app/containers/Guest.detail.js
+++ src/Share/src/app/containers/Guest.detail.js
@@ -13,9 +13,44 @@ class Detail extends React.Component{
     }
     componentDidMount(){
         const { dispatch,location,routeParams} = this.props;
-
-
     }
+    componentDidUpdate(){
+        var profile = this.refs.profile;
+        var hh = $(profile).outerHeight();
+        var moreInfo = this.refs.moreInfo;
+        var moreBtn = this.refs.moreBtn;
+        if(hh<40){
+            $(moreInfo).hide();
+            $(profile).css({
+                "display":"block",
+                "height":hh,
+            });
+        }else{
+            $(profile).css("display","-webkit-box");
+            var hh2 = $(profile).outerHeight();
+            $(profile).css({
+                "height":hh2,
+            });
+            $(moreInfo).show();
+            $(moreInfo).on("click",function () {
+                if($(moreInfo).text()=="查看更多"){
+                    $(moreInfo).text("收起");
+                    $(profile).css({
+                        "display":"block",
+                        "height":hh,
+                    });
+                }else if($(moreInfo).text()=="收起"){
+                    // var hh2 = $(profile).outerHeight();
+                    $(moreInfo).text("查看更多");
+                    $(profile).css({
+                        "display":"-webkit-box",
+                        "height":hh2,
+                    });
+                }
+            });
+        }
+    }
+
     renderCompany(){
         const { dispatch,location,guestDetailData } = this.props;
         if(guestDetailData.data.company){
@@ -61,14 +96,14 @@ class Detail extends React.Component{
                             </div>
                             <div className="myLine"></div>
                             <div className="guest-profile">
-                                <div className="guest-myprofile">个人简介：{`${guestDetailData.data.guest.content}`}</div>
-                                <div className="guest-more">
-                                    <div className="guest-more-word">查看更多</div>
-                                    <div className="guest-more-arrow">
-                                        <svg className="haloIcon" aria-hidden="true">
-                                            <use xlinkHref="#haloIcon-arrow"></use>
-                                        </svg>
-                                    </div>
+                                <div className="guest-myprofile" ref="profile">个人简介：{`${guestDetailData.data.guest.content}`}</div>
+                                <div className="guest-more" ref="moreInfo">
+                                    <div className="guest-more-word" ref="moreBtn">查看更多</div>
+                                    {/*<div className="guest-more-arrow">*/}
+                                        {/*<svg className="haloIcon" aria-hidden="true">*/}
+                                            {/*<use xlinkHref="#haloIcon-arrow"></use>*/}
+                                        {/*</svg>*/}
+                                    {/*</div>*/}
                                 </div>
                             </div>
                         </div>
