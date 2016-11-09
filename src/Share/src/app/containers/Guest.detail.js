eb39db7738ec92be8d2c6832a722448e43cac5b3
野猪yz <1746534101@qq.com>
2016-11-09 17:42:45 +0800

    嘉宾详情，细节

--- src/Share/src/app/containers/Guest.detail.js
+++ src/Share/src/app/containers/Guest.detail.js
@@ -1,9 +1,13 @@
 import CommonHeader from './Common.header'
 import CommonArticle from './Common.article'
 import CommonVideos from './Common.videos'
-import {fetchGuestDetailIfNeeded} from '../actions/guest.detail'
+import {fetchGuestDetailIfNeeded,destroyGuestDetailData} from '../actions/guest.detail'
 let Link=ReactRouter.Link;
 class Detail extends React.Component{
+    constructor(props) {
+        super(props);
+        this.isWechatInit=false
+    }
     componentWillMount(){
         const { dispatch,location,routeParams} = this.props;
         dispatch(fetchGuestDetailIfNeeded({
@@ -12,8 +16,10 @@ class Detail extends React.Component{
     }
     componentDidMount(){
         const { dispatch,location,routeParams} = this.props;
-
-
+    }
+    componentWillUnmount(){
+        const { dispatch ,routeParams} = this.props
+        dispatch(destroyGuestDetailData());
     }
     renderCompany(){
         const { dispatch,location,guestDetailData } = this.props;
@@ -35,6 +41,7 @@ class Detail extends React.Component{
             )
         }
     }
+
     render() {
             const { dispatch,location,guestDetailData } = this.props;
             // console.log(guestDetailData);
@@ -45,6 +52,18 @@ class Detail extends React.Component{
                     </div>
                 )
             }else if(guestDetailData.data){
+                // console.log(this.isWechatInit)
+                // console.log(guestDetailData.data.guest.title,guestDetailData.data.guest.content,guestDetailData.data.guest.avatar_url)
+                if(!this.isWechatInit){
+                    app.wechat.init({
+                        title: `${guestDetailData.data.guest.title}`,
+                        content: `${guestDetailData.data.guest.content}`,
+                        logo : `${guestDetailData.data.guest.avatar_url}?imageView2/1/w/200/h/200`,
+                        link : window.location.href,
+                    });
+                    this.isWechatInit=true;
+                }
+
                 return (
                     <div>
                         <CommonHeader title="个人主页" ></CommonHeader>
