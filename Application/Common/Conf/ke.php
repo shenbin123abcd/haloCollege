d8d4e8240e3991c6958b3542ff8e27875adff04e
shenbin <846697463@qq.com>
2016-11-24 15:05:51 +0800

    delete

--- Application/Common/Conf/ke.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-	//课程分类
-	'COURSE_CATE'=>array(1=>'公开课', 2=>'培训营', 3=>'合作伙伴'),
-
-);
--- Application/Common/Conf/user.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-	// 用户组
-	'USER_GROUP'=>array(0=>'普通用户', 1=>'VIP'),
-
-);
